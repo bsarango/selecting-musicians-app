a4189913f5e5a1d9f16070ba643707c0eb2d907e
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-06-26 01:23:08 -0400

    DOMContentLoaded event adjusted to alert, selectMusician function moves node to band container, css added to header, musician nodes, and band container, background color for body

---
+++ src/index.js
@@ -0,0 +1,65 @@
+//Code for Selecting Musicians App
+
+//Add event listener to buttons for submit
+
+document.addEventListener("DOMContentLoaded",(e)=> alert("Select your band!"))
+
+function manageMusicianInput(e){
+    e.preventDefault()
+    const musicianInputInfo=document.querySelectorAll("input")
+    const musicianObj = {
+        name: musicianInputInfo[0].value,
+        image: musicianInputInfo[1].value,
+        instrument: musicianInputInfo[2].value,
+    }
+    addNewMusician(musicianObj)
+}
+
+function addNewMusician(musicianObj){
+    fetch("http://localhost:3000/musicians",{
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body:JSON.stringify(musicianObj)
+    })
+    .then(resp => resp.json())
+    .then(musicianData => renderMusician(musicianData))
+} 
+
+function renderMusician(musicianObj){
+    const musicianContainer = document.querySelector("#selection-of-musicians")
+    let newMusician = document.createElement("div")
+    newMusician.className = "musician"
+    newMusician.innerHTML=`
+        <h2> ${musicianObj.name} </h2>
+        <img src= "${musicianObj.image}" class = "musician-pic"/>
+        <h3> Instrument : ${musicianObj.instrument} </h3>
+        <button type = "button"  class="select-btn" > x </button> 
+    `
+    
+    newMusician.querySelector(".select-btn").addEventListener("click",selectMusician)
+    
+    musicianContainer.appendChild(newMusician)
+}
+
+function selectMusician(e){
+    const currentMusicians = document.querySelectorAll(".musician")
+    const selectedMusician = e.target.parentNode
+    const bandContainer = document.querySelector("#selected-band")
+    let newMember = document.createElement("div")
+    newMember = selectedMusician
+    bandContainer.appendChild(selectedMusician)
+}
+function renderAllMusicians(){
+    fetch("http://localhost:3000/musicians")
+    .then(resp=>resp.json())
+    .then(musicians=>{
+        musicians.forEach(musician=>renderMusician(musician))
+    })
+}
+
+const inputBtns = document.querySelector(".add-musician-form")
+inputBtns.addEventListener("submit", manageMusicianInput)
+
+renderAllMusicians()
